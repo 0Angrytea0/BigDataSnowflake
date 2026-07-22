73fabdddc81366546bb7738e8238fc00e4d2b38f
0Angrytea0 <116645263+0Angrytea0@users.noreply.github.com>
2026-07-22 12:45:24 +0300

    Update insert_dim_seller.sql

--- dml/insert_dim_seller.sql
+++ dml/insert_dim_seller.sql
@@ -1,5 +1,6 @@
 INSERT INTO dim_seller (
   seller_id,
+  source_file,
   first_name,
   last_name,
   email,
@@ -8,6 +9,7 @@ INSERT INTO dim_seller (
 )
 SELECT
   sale_seller_id    AS seller_id,
+  source_file,
   seller_first_name AS first_name,
   seller_last_name  AS last_name,
   seller_email      AS email,
@@ -16,17 +18,18 @@ SELECT
 FROM (
   SELECT
     sale_seller_id,
+    source_file,
     seller_first_name,
     seller_last_name,
     seller_email,
     seller_country,
     seller_postal_code,
     ROW_NUMBER() OVER (
-      PARTITION BY sale_seller_id
+      PARTITION BY source_file, sale_seller_id
       ORDER BY sale_date
     ) AS rn
   FROM mock_data
 ) t
 WHERE t.rn = 1
   AND sale_seller_id IS NOT NULL
-ON CONFLICT (seller_id) DO NOTHING;
+ON CONFLICT (source_file, seller_id) DO NOTHING;
