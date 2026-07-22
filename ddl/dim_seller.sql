7ba28f8b0e4231345c339ba0f87f15b288aead00
0Angrytea0 <116645263+0Angrytea0@users.noreply.github.com>
2026-07-22 21:29:43 +0300

    Update dim_seller.sql

--- ddl/dim_seller.sql
+++ ddl/dim_seller.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS dim_seller;
 CREATE TABLE dim_seller (
   seller_sk      SERIAL PRIMARY KEY,
   source_file    TEXT NOT NULL,
-  seller_id      BIGINT   UNIQUE,      
+  seller_id      BIGINT   NOT NULL,      
   first_name     TEXT     NOT NULL,
   last_name      TEXT     NOT NULL,
   email          TEXT,
