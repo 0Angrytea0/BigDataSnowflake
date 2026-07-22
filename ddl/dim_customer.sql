be4c8498df75e0da62e62665fd61860ec43cd879
0Angrytea0 <116645263+0Angrytea0@users.noreply.github.com>
2026-07-22 21:25:13 +0300

    Update dim_customer.sql

--- ddl/dim_customer.sql
+++ ddl/dim_customer.sql
@@ -2,12 +2,12 @@ DROP TABLE IF EXISTS dim_customer;
 CREATE TABLE dim_customer (
   customer_sk    SERIAL PRIMARY KEY,
   source_file    TEXT   NOT NULL,
-  customer_id    BIGINT,     
+  customer_id    BIGINT NOT NULL,     
   first_name     TEXT   NOT NULL,
   last_name      TEXT   NOT NULL,
   age            INT,
   email          TEXT,
   country        TEXT,
-  postal_code    TEXT
+  postal_code    TEXT,
   UNIQUE (source_file, customer_id)
 );
